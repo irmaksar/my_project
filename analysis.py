ac5535cb756e11f34158d5a906898739637dc7ec
irmaksar <36905573+irmaksar@users.noreply.github.com>
2018-03-01 11:11:36 -0800

    add index column as country

--- analysis.py
+++ analysis.py
@@ -6,4 +6,4 @@ Created on Thu Mar  1 09:54:56 2018
 """
 
 import pandas as pd
-oceania = pd.read_csv ('C:\\Users\\irmak\\Desktop\\data\\gapminder_gdp_oceania.csv')
+oceania = pd.read_csv ('C:\\Users\\irmak\\Desktop\\data\\gapminder_gdp_oceania.csv', index_col='country')
