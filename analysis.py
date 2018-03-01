7d490bb1ad4dedc23df8cf35a2338151d632153d
Irmak SARGIN <irmak.sargin@wsu.edu>
2018-03-01 10:12:33 -0800

    create .py script

---
+++ analysis.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  1 09:54:56 2018
+
+@author: irmak
+"""
+
+import pandas as pd
+data = pd.read_csv ('C:\\Users\\irmak\\Desktop\\data\\gapminder_gdp_europe.csv')
